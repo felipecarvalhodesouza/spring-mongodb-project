48392839817eb92f138cbb1b863ad0ed50a55ca3
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2019-02-17 10:03:33 -0300

    Inserção de usuário com POST

--- src/main/java/com/felipecarvalho/projectSpringMongo/services/UserService.java
+++ src/main/java/com/felipecarvalho/projectSpringMongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipecarvalho.projectSpringMongo.domain.User;
+import com.felipecarvalho.projectSpringMongo.dto.UserDTO;
 import com.felipecarvalho.projectSpringMongo.repository.UserRepository;
 import com.felipecarvalho.projectSpringMongo.services.exception.ObjectNotFoundException;
 
@@ -17,13 +18,22 @@ public class UserService {
 	private UserRepository repo;
 
 	public List<User> findAll() {
-
 		return repo.findAll();
 	}
 
 	public User findById(String id) {
-
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(
+				objDto.getId(),
+				objDto.getName(),
+				objDto.getEmail());
 	}
 }
