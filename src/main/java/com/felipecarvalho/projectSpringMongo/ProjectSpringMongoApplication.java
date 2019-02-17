51e091e13f9bdb47b7add83c8ffb2ab97b69d986
Felipe Carvalho <desouzafelipecarvalho@gmail.com>
2019-02-17 08:03:21 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/felipecarvalho/projectSpringMongo/ProjectSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.felipecarvalho.projectSpringMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipecarvalho/projectSpringMongo/ProjectSpringMongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.felipecarvalho.projectSpringMongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectSpringMongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
